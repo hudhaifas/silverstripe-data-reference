4655c7374ab2957752ddfe739f4337e327b07aae
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2018-07-14 11:50:06 -0500

    - Fixed minor bug

--- src/Api/ReferenceService.php
+++ src/Api/ReferenceService.php
@@ -82,12 +82,12 @@ class ReferenceService
         $items[] = HiddenField::create('OwnerClass', 'OwnerClass', $ownerClass);
         $items[] = HiddenField::create('OwnerID', 'OwnerID', $ownerID);
 
-        $items[] = TextField::create('Name', _t('References.NAME', 'Name'));
-        $items[] = TextField::create('Description', _t('References.DESCRIPTION', 'Description'));
-        $items[] = TextField::create('Details', _t('References.DETAILS', 'Details'));
-        $items[] = NumericField::create('Page', _t('References.PAGE', 'Page'));
-        $items[] = TextField::create('Link', _t('References.LINK', 'Link'));
-        $items[] = DateField::create('Date', _t('References.DATE', 'Date'));
+        $items[] = TextField::create('Name', _t('Reference.NAME', 'Name'));
+        $items[] = TextField::create('Description', _t('Reference.DESCRIPTION', 'Description'));
+        $items[] = TextField::create('Details', _t('Reference.DETAILS', 'Details'));
+        $items[] = NumericField::create('Page', _t('Reference.PAGE', 'Page'));
+        $items[] = TextField::create('Link', _t('Reference.LINK', 'Link'));
+        $items[] = DateField::create('Date', _t('Reference.DATE', 'Date'));
 
         return $items;
     }
